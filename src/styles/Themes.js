3df2c3c89a140ff9c66d434df49ec9bf75b7fc8f
mustafa <aalsahek@gmail.com>
2022-04-18 15:27:58 +0300

    styling-1

---
+++ src/styles/Themes.js
@@ -0,0 +1,19 @@
+// add themes as per your requirments
+export const dark = {
+  body: "#202020", //black shade
+  text: "#fff",
+  bodyrgba: "32,32,32",
+  textRgba: "255,255,255",
+  grey: "#bebebe", //grey shade
+
+  fontxs: "0.75em",
+  fontsm: "0.875em",
+  fontmd: "1em",
+  fontlg: "1.2em",
+  fontxl: "2em",
+  fontxxl: "3em",
+  fontxxxl: "5em",
+  fontBig: "10em",
+
+  navHeight: "5rem",
+};
